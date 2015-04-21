948226e42a2a4f200185be24a0add7a51229e08c
MattCordeiro <matthew3cord@gmail.com>
2015-04-21 10:54:50 -0700

    Working grid.

--- public/app.js
+++ public/app.js
@@ -4,23 +4,6 @@ angular
       'ngResource',
       'wu.masonry'
    ]);
-angular
-   .module('SubDig')
-   .controller('AppCtrl', ['$scope', function($scope, $resource, ProjectFct){
-      $scope.testing = 'Subtle Loves Angular!';
-      ProjectFct.get(function(data){
-         $scope.projects = data.projects;
-         console.log(data.projects);
-      });
-}]);
-
-
-angular
-   .module('SubDig')
-   .config(["$routeProvider", function($routeProvider){
-      $routeProvider
-         .when('/portfolio.html', {controller: 'AppCtr', templateUrl: '/partials/grid.html'});
-   }]);
 
 
 
@@ -29,4 +12,81 @@ angular
       .factory('ProjectFct',
          ["$resource", function($resource){
             return $resource('/app/data/projects.json');
+   }]);
+
+angular
+   .module('SubDig')
+   .controller('AppCtrl', ['$scope', function(){
+      var vm = this;
+      vm.testing = 'Subtle Loves Angular!';
+      vm.projects = [
+            {
+               "src"   : "fullpixel.png",
+               "title" : "FullPixel",
+               "desc"  : ""
+            },
+            {
+               "src"   : "tofurky.png",
+               "title" : "Tofurky",
+               "desc"  : ""
+            },
+            {
+               "src"   : "juniperfund.png",
+               "title" : "The Juniper Fund",
+               "desc"  : ""
+            },
+            {
+               "src"   : "intelliskin.png",
+               "title" : "Intelliskin",
+               "desc"  : ""
+            },
+            {
+               "src"   : "wellpartner.png",
+               "title" : "Wellpartner",
+               "desc"  : ""
+            },
+            {
+               "src"   : "wafed.png",
+               "title" : "Washington Federal",
+               "desc"  : ""
+            },
+            {
+               "src"   : "skylakesc.png",
+               "title" : "Sky Lakes Medical Center",
+               "desc"  : ""
+            },
+            {
+               "src"   : "nedc.png",
+               "title" : "New England Design & Construction",
+               "desc"  : ""
+            },
+            {
+               "src"   : "orbreakers.png",
+               "title" : "Oregon Breakers",
+               "desc"  : ""
+            },
+            {
+               "src"   : "svelte.png",
+               "title" : "CalNaturale Svelte",
+               "desc"  : ""
+            },
+            {
+               "src"   : "earthwise.png",
+               "title" : "Earthwise Salvage",
+               "desc"  : ""
+            },
+            {
+               "src"   : "windingwaters.png",
+               "title" : "Winding Waters Rafting",
+               "desc"  : ""
+            }
+         ];
+}]);
+
+
+angular
+   .module('SubDig')
+   .config(["$routeProvider", function($routeProvider){
+      $routeProvider
+         .when('/portfolio.html', {controller: 'AppCtr', templateUrl: '/partials/grid.html'});
    }]);
