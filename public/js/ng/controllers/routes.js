f7c61dbc3e507b121f35d55fd41db046a84bfa33
MattCordeiro <matthew3cord@gmail.com>
2015-04-22 10:57:01 -0700

    Working on views.

--- public/js/ng/controllers/routes.js
+++ public/js/ng/controllers/routes.js
@@ -2,5 +2,16 @@ angular
    .module('SubDig')
    .config(function($routeProvider){
       $routeProvider
-         .when('/portfolio.html', {controller: 'AppCtr', templateUrl: '/partials/grid.html'});
+         .when('/portfolio.html', {controller: 'AppCtr', templateUrl: '/partials/grid.html'})
+         .when('/earthwise', {controller: 'AppCtr', templateUrl: 'partials/earthw.html'})
+         .when('/fullpixel', {controller: 'AppCtr', templateUrl: 'partials/fullpix.html'})
+         .when('/intelliskin', {controller: 'AppCtr', templateUrl: 'partials/intelli.html'})
+         .when('/juniperfund', {controller: 'AppCtr', templateUrl: 'partials/juniper.html'})
+         .when('/nedc', {controller: 'AppCtr', templateUrl: 'partials/nedc.html'})
+         .when('/oregonbreakers', {controller: 'AppCtr', templateUrl: 'partials/orbr.html'})
+         .when('/skylakes', {controller: 'AppCtr', templateUrl: 'partials/skylakes.html'})
+         .when('/tofurky', {controller: 'AppCtr', templateUrl: 'partials/tofurky.html'})
+         .when('/wafed', {controller: 'AppCtr', templateUrl: 'partials/wafed.html'})
+         .when('/wellpartner', {controller: 'AppCtr', templateUrl: 'partials/wellp.html'})
+         .when('/windingwaters', {controller: 'AppCtr', templateUrl: 'partials/winwat.html'});
    });
