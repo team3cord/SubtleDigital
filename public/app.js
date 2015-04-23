1dac1f12e2ecb2ccf4570cff9dc2403645786a2d
MattCordeiro <matthew3cord@gmail.com>
2015-04-22 22:02:06 -0700

    Cleaning up project data set.

--- public/app.js
+++ public/app.js
@@ -8,79 +8,66 @@ angular
    .module('SubDig')
    .controller('AppCtrl', ['$scope', function(){
       var vm = this;
-      vm.testing = 'Subtle Loves Angular!';
       vm.projects = [
             {
                "path"  : "fullpixel",
                "src"   : "fullpixel.png",
-               "title" : "FullPixel",
-               "desc"  : ""
+               "title" : "FullPixel"
             },
             {
                "path"  : "tofurky",
                "src"   : "tofurky.png",
-               "title" : "Tofurky",
-               "desc"  : ""
+               "title" : "Tofurky"
             },
             {
                "path"  : "thejuniperfund",
                "src"   : "juniperfund.png",
-               "title" : "The Juniper Fund",
-               "desc"  : ""
+               "title" : "The Juniper Fund"
             },
             {
                "path"  : "intelliskin",
                "src"   : "intelliskin.png",
-               "title" : "Intelliskin",
-               "desc"  : ""
+               "title" : "Intelliskin"
             },
             {
                "path"  : "wellpartner",
                "src"   : "wellpartner.png",
-               "title" : "Wellpartner",
-               "desc"  : ""
+               "title" : "Wellpartner"
             },
             {
                "path"  : "washingtonfederal",
                "src"   : "wafed.png",
-               "title" : "Washington Federal",
-               "desc"  : ""
+               "title" : "Washington Federal"
             },
             {
                "path"  : "skylakesmedicalcenter",
                "src"   : "skylakesc.png",
-               "title" : "Sky Lakes Medical Center",
-               "desc"  : ""
+               "title" : "Sky Lakes Medical Center"
             },
             {
                "path"  : "nedc",
                "src"   : "nedc.png",
-               "title" : "New England Design & Construction",
-               "desc"  : ""
+               "title" : "New England Design & Construction"
             },
             {
                "path"  : "oreogonbreakers",
                "src"   : "orbreakers.png",
-               "title" : "Oregon Breakers",
-               "desc"  : ""
+               "title" : "Oregon Breakers"
             },
             {
                "path"  : "calnaturale",
                "src"   : "svelte.png",
-               "title" : "CalNaturale Svelte",
-               "desc"  : ""
+               "title" : "CalNaturale Svelte"
             },
             {
                "path"  : "earthwise",
                "src"   : "earthwise.png",
-               "title" : "Earthwise Salvage",
-               "desc"  : ""
+               "title" : "Earthwise Salvage"
             },
             {
                "path"  : "windingwatersrafting",
                "src"   : "windingwaters.png",
-               "title" : "Winding Waters Rafting",
-               "desc"  : ""
+               "title" : "Winding Waters Rafting"
             }
          ];
 }]);
